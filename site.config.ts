42c6624fa22dbdfc6428ab8a5451ec356a4dde85
Slendy_Milky <slendymilky@gmail.com>
2024-07-11 20:34:29 +0200

    yes

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '067dd719a912471ea9a3ac10710e7fdf',
+  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
