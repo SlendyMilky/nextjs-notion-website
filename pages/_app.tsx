120291021ccba723079733723509fde5c76d9345
Slendy_Milky <slendymilky@gmail.com>
2024-10-11 09:01:08 +0200

    Revert "ajout analytics"
    This reverts commit 1c38fc75711a035e6b63a755c826e653980d56b9.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
+// global styles shared across the entire site
 import * as React from 'react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+
 import * as Fathom from 'fathom-client'
+// used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
 import posthog from 'posthog-js'
+// used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-coy.css'
+// core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 import 'styles/global.css'
+// this might be better for dark mode
+// import 'prismjs/themes/prism-okaidia.css'
+// global style overrides for notion
 import 'styles/notion.css'
+// global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
+
 import { bootstrap } from '@/lib/bootstrap-client'
 import { fathomConfig, fathomId, isServer, posthogConfig, posthogId } from '@/lib/config'
 import { Analytics } from '@vercel/analytics/react'
@@ -46,23 +56,9 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  React.useEffect(() => {
-    var _paq = window._paq = window._paq || [];
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-      var u="//pulse.slyc.ch/";
-      _paq.push(['setTrackerUrl', u+'matomo.php']);
-      _paq.push(['setSiteId', '2']);
-      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-    })();
-  }, []);
-
   return(
     <>
       <Component {...pageProps} />
       <Analytics />
-    </>
-  )
+    </>)
 }
